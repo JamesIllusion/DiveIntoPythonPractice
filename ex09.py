3aa8a33bbadb992888ce0910bef74fb17a384a00
JamesIllusion <james.lt.xu@gmail.com>
2015-08-15 20:40:11 -0500

    20150815 Make changes from Fortress_Win8

--- ex09.py
+++
@@ -1,6 +0,0 @@
-days = "Mon, Tue, Wed, Thu, Fri, Sat, Sun"
-months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec\n"
-
-print "Here are the days:", days
-print "Here are the months:", months
-
--- ex10.py
+++
@@ -1,7 +0,0 @@
-tabby_cat = "\tI'm tabbed in."
-persian_cat = "I'm split\non a line."
-backslash_cat = "I'm \\ a \\ cat."
-
-print tabby_cat
-print persian_cat
-print backslash_cat
